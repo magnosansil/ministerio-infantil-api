27d3ccc30f2d2c5f7931578b205b02bef539f0c2
magnosansil <magnosansil@outlook.com>
2024-11-05 22:45:39 -0300

    Updates Verifiers

--- src/utils/validadores.ts
+++ src/utils/validadores.ts
@@ -1,3 +1,10 @@
+import {
+  verificarExistenciaCpfCrianca,
+  verificarExistenciaCpfResponsavel,
+  verificarExistenciaDoenca,
+  verificarExistenciaRestricao,
+  verificarExistenciaTurma,
+} from "../database/criancaQueries";
 import { Responsavel } from "../types";
 import { validarCPF } from "./validarCPF";
 import { Response } from "express";
@@ -11,13 +18,115 @@ export const verificarCPF = (cpf: string, res: Response): boolean => {
 };
 
 export const validarCamposResponsavel = (
-  body: Responsavel,
+  body: Partial<Responsavel>,
   res: Response
 ): boolean => {
-  const { cpf, nome, telefone, relacionamento, idade } = body;
+  const { cpf, nome, telefone, data_nascimento, rg, cep, endereco, sexo } =
+    body;
 
-  if (!cpf || !nome || !telefone || !relacionamento || !idade) {
-    res.status(400).json({ error: "Todos os campos são obrigatórios." });
+  if (
+    !cpf ||
+    !nome ||
+    !telefone ||
+    !data_nascimento ||
+    !rg ||
+    !cep ||
+    !endereco ||
+    !sexo
+  ) {
+    res
+      .status(400)
+      .json({ error: "Todos os campos obrigatórios estão ausentes." });
+    return false;
+  }
+
+  return true;
+};
+
+export const validarDoenca = async (
+  id_doenca: number,
+  res: Response
+): Promise<boolean> => {
+  const existeDoenca = await verificarExistenciaDoenca(id_doenca);
+  if (!existeDoenca) {
+    res
+      .status(400)
+      .json({ error: "A doença informada não existe no sistema." });
+    return false;
+  }
+  return true;
+};
+
+export const validarRestricao = async (
+  id_restricao: number,
+  res: Response
+): Promise<boolean> => {
+  const existeRestricao = await verificarExistenciaRestricao(id_restricao);
+  if (!existeRestricao) {
+    res
+      .status(400)
+      .json({ error: "A restrição informada não existe no sistema." });
+    return false;
+  }
+  return true;
+};
+
+export const validarCPFDaResponsavel = async (cpf: string, res: Response): Promise<boolean> => {
+  if (cpf && !validarCPF(cpf)) {
+    res.status(400).json({ error: "CPF do responsável inválido." });
+    return false;
+  }
+
+  const cpfResponsavelExiste = await verificarExistenciaCpfResponsavel(cpf);
+  if (cpfResponsavelExiste) {
+    res.status(400).json({ error: "CPF já cadastrado como responsável." });
+    return false;
+  }
+
+  const cpfCriancaExiste = await verificarExistenciaCpfCrianca(cpf);
+  if (cpfCriancaExiste) {
+    res.status(400).json({ error: "CPF não pode ser o mesmo de uma criança." });
+    return false;
+  }
+
+  return true;
+};
+
+export const validarCPFDaCrianca = async (
+  cpf: string,
+  res: Response
+): Promise<boolean> => {
+  if (cpf && !validarCPF(cpf)) {
+    res.status(400).json({ error: "CPF da criança inválido." });
+    return false;
+  }
+
+  const cpfCriancaExiste = await verificarExistenciaCpfCrianca(cpf);
+  if (cpfCriancaExiste) {
+    res.status(400).json({ error: "CPF da criança já está cadastrado." });
+    return false;
+  }
+
+  const cpfResponsavelExiste = await verificarExistenciaCpfResponsavel(cpf);
+  if (cpfResponsavelExiste) {
+    res
+      .status(400)
+      .json({
+        error: "CPF da criança não pode ser o mesmo de um responsável.",
+      });
+    return false;
+  }
+
+  return true;
+};
+
+export const validarTurma = async (
+  id_turma: number,
+  res: Response
+): Promise<boolean> => {
+  const existeTurma = await verificarExistenciaTurma(id_turma);
+  if (!existeTurma) {
+    res.status(400).json({ error: "A turma informada não existe no sistema." });
     return false;
   }
   return true;
