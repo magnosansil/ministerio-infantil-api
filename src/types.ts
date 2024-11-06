44dec8f7c70710b632c02c5c23655d37411eb5d7
magnosansil <magnosansil@outlook.com>
2024-11-05 22:45:18 -0300

    Updates Responsavel Interface And Creates Crianca Interface

--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,26 @@ export interface Responsavel {
   cpf: string;
   nome: string;
   telefone: string;
-  relacionamento: string;
-  idade: number;
+  data_nascimento: Date;
+  rg: string;
+  cep: string;
+  endereco: string;
+  sexo: "M" | "F" | "Outro";
+  email?: string | null;
+}
+export interface Crianca {
+  id: number;
+  nome: string;
+  data_nascimento: Date;
+  tipo_sanguineo?: string | null;
+  id_doenca?: number | null;
+  id_restricao?: number | null;
+  id_turma?: number | null;
+  cpf?: string | null;
+  rg?: string | null;
+  fk_cpf_responsavel: string;
+  relacionamento_1: string; 
+  cpf_responsavel_2?: string | null; 
+  relacionamento_2?: string | null; 
+  sexo: "M" | "F" | "Outro"; 
 }
